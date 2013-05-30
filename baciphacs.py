d30a293de4becd63199ca0c1f784e5fb87d2bd56
Thibauld Nion <thibauld@tibonihoo.net>
2013-05-30 23:13:08 +0200

    add generation of a full bar chart

--- baciphacs.py
+++ baciphacs.py
@@ -25,12 +25,32 @@ def GenerateHTMLHorizontalBar(relWidth,relErrorWidth):
   secondPartWidth = relWidth-firstPartWidth
   return """\
 <table cellspacing="0" cellpadding="0" border="0" style="width:100%%">
-  <tr>
-    <td style="width:%.0f%%;height:1ex;background-color:blue;"></td>
-    <td style="width:%.0f%%;height:1ex;background-color:blue;text-align:left">|</td>
-    <td style="width:%.0f%%;height:1ex;text-align:right">|</td>
-    <td></td>
-  </tr>
+<tr>
+  <td style="width:%.0f%%;height:1ex;background-color:blue;"></td>
+  <td style="width:%.0f%%;height:1ex;background-color:blue;text-align:left">|</td>
+  <td style="width:%.0f%%;height:1ex;text-align:right">|</td>
+  <td></td>
+</tr>
 </table>""" % (100*firstPartWidth,100*secondPartWidth,100*relErrorWidth)
 
-  
+def GenerateHTMLLabelledRow(label,htmlRowData):
+  """Generate a <tr> row with the given label and data.
+  'label' a string to be set at the begining of the row.
+  'htmlRowData' the html content to be put next to the label (in a <td> tag)
+  Note: the htmlRowData will be indented appropriately.
+  """
+  return """\
+<tr>
+  <th style="margin-top:.2ex;padding-right:1ex;text-align:right;">%s</th>
+  <td style="margin-top:.2ex;">
+%s
+  </td>
+</tr>""" % (label,"\n".join("    %s"%line for line in htmlRowData.splitlines()))
+
+def GenerateHTMLHorizontalBarChart(dataSamples,numStdev):
+  norm = max(ds.value+(numStdev*ds.stdev) for ds in dataSamples)
+  bars = [ GenerateHTMLHorizontalBar(float(d.value)/norm,float(numStdev*d.stdev)/norm) for d in dataSamples ]
+  return """\
+<table cellspacing="0" cellpadding="0" border="0" style="width:50ex">
+%s
+</table>""" % "\n".join([GenerateHTMLLabelledRow(d.label,b) for b in bars])
--- test.py
+++ test.py
@@ -4,21 +4,23 @@
 import unittest
 from baciphacs import DataSample
 from baciphacs import GenerateHTMLHorizontalBar
+from baciphacs import GenerateHTMLLabelledRow
+from baciphacs import GenerateHTMLHorizontalBarChart
+
 
 class TestGenerateHTMLHorizontalBar(unittest.TestCase):
-
   
   def test_generate_bar_with_values_is_ok(self):
     htmlSnippet = GenerateHTMLHorizontalBar(.6,.1)
     self.assertEqual(
       """\
 <table cellspacing="0" cellpadding="0" border="0" style="width:100%">
-  <tr>
-    <td style="width:50%;height:1ex;background-color:blue;"></td>
-    <td style="width:10%;height:1ex;background-color:blue;text-align:left">|</td>
-    <td style="width:10%;height:1ex;text-align:right">|</td>
-    <td></td>
-  </tr>
+<tr>
+  <td style="width:50%;height:1ex;background-color:blue;"></td>
+  <td style="width:10%;height:1ex;background-color:blue;text-align:left">|</td>
+  <td style="width:10%;height:1ex;text-align:right">|</td>
+  <td></td>
+</tr>
 </table>""",
       htmlSnippet)
 
@@ -27,12 +29,12 @@ class TestGenerateHTMLHorizontalBar(unittest.TestCase):
     self.assertEqual(
       """\
 <table cellspacing="0" cellpadding="0" border="0" style="width:100%">
-  <tr>
-    <td style="width:0%;height:1ex;background-color:blue;"></td>
-    <td style="width:10%;height:1ex;background-color:blue;text-align:left">|</td>
-    <td style="width:30%;height:1ex;text-align:right">|</td>
-    <td></td>
-  </tr>
+<tr>
+  <td style="width:0%;height:1ex;background-color:blue;"></td>
+  <td style="width:10%;height:1ex;background-color:blue;text-align:left">|</td>
+  <td style="width:30%;height:1ex;text-align:right">|</td>
+  <td></td>
+</tr>
 </table>""",
       htmlSnippet)
     
@@ -56,12 +58,12 @@ class TestGenerateHTMLHorizontalBar(unittest.TestCase):
     self.assertEqual(
       """\
 <table cellspacing="0" cellpadding="0" border="0" style="width:100%">
-  <tr>
-    <td style="width:10%;height:1ex;background-color:blue;"></td>
-    <td style="width:0%;height:1ex;background-color:blue;text-align:left">|</td>
-    <td style="width:0%;height:1ex;text-align:right">|</td>
-    <td></td>
-  </tr>
+<tr>
+  <td style="width:10%;height:1ex;background-color:blue;"></td>
+  <td style="width:0%;height:1ex;background-color:blue;text-align:left">|</td>
+  <td style="width:0%;height:1ex;text-align:right">|</td>
+  <td></td>
+</tr>
 </table>""",
       htmlSnippet)
 
@@ -70,12 +72,12 @@ class TestGenerateHTMLHorizontalBar(unittest.TestCase):
     self.assertEqual(
       """\
 <table cellspacing="0" cellpadding="0" border="0" style="width:100%">
-  <tr>
-    <td style="width:0%;height:1ex;background-color:blue;"></td>
-    <td style="width:0%;height:1ex;background-color:blue;text-align:left">|</td>
-    <td style="width:30%;height:1ex;text-align:right">|</td>
-    <td></td>
-  </tr>
+<tr>
+  <td style="width:0%;height:1ex;background-color:blue;"></td>
+  <td style="width:0%;height:1ex;background-color:blue;text-align:left">|</td>
+  <td style="width:30%;height:1ex;text-align:right">|</td>
+  <td></td>
+</tr>
 </table>""",
       htmlSnippet)
     
@@ -84,12 +86,12 @@ class TestGenerateHTMLHorizontalBar(unittest.TestCase):
     self.assertEqual(
       """\
 <table cellspacing="0" cellpadding="0" border="0" style="width:100%">
-  <tr>
-    <td style="width:100%;height:1ex;background-color:blue;"></td>
-    <td style="width:0%;height:1ex;background-color:blue;text-align:left">|</td>
-    <td style="width:0%;height:1ex;text-align:right">|</td>
-    <td></td>
-  </tr>
+<tr>
+  <td style="width:100%;height:1ex;background-color:blue;"></td>
+  <td style="width:0%;height:1ex;background-color:blue;text-align:left">|</td>
+  <td style="width:0%;height:1ex;text-align:right">|</td>
+  <td></td>
+</tr>
 </table>""",
       htmlSnippet)
 
@@ -98,13 +100,65 @@ class TestGenerateHTMLHorizontalBar(unittest.TestCase):
     self.assertEqual(
       """\
 <table cellspacing="0" cellpadding="0" border="0" style="width:100%">
-  <tr>
-    <td style="width:0%;height:1ex;background-color:blue;"></td>
-    <td style="width:0%;height:1ex;background-color:blue;text-align:left">|</td>
-    <td style="width:100%;height:1ex;text-align:right">|</td>
-    <td></td>
-  </tr>
+<tr>
+  <td style="width:0%;height:1ex;background-color:blue;"></td>
+  <td style="width:0%;height:1ex;background-color:blue;text-align:left">|</td>
+  <td style="width:100%;height:1ex;text-align:right">|</td>
+  <td></td>
+</tr>
+</table>""",
+      htmlSnippet)
+
+
+class TestGenerateHTMLLabelledRow(unittest.TestCase):
+
+  def test_generate_row_with_label_is_ok(self):
+    htmlSnippet = GenerateHTMLLabelledRow("my_label","MARKER")
+    self.assertEqual(
+      """\
+<tr>
+  <th style="margin-top:.2ex;padding-right:1ex;text-align:right;">my_label</th>
+  <td style="margin-top:.2ex;">
+    MARKER
+  </td>
+</tr>""",
+      htmlSnippet)
+
+  def test_generate_row_with_label_and_multine_data_is_indented_correctly(self):
+    htmlSnippet = GenerateHTMLLabelledRow("my_label","MARKER\nOTHER")
+    self.assertEqual(
+      """\
+<tr>
+  <th style="margin-top:.2ex;padding-right:1ex;text-align:right;">my_label</th>
+  <td style="margin-top:.2ex;">
+    MARKER
+    OTHER
+  </td>
+</tr>""",
+      htmlSnippet)
+
+class TestGenerateHTMLHorizontalBarChart(unittest.TestCase):
+
+  def test_generate_bar_chart_with_single_sample(self):
+    htmlSnippet = GenerateHTMLHorizontalBarChart([DataSample("my_label",6,1)],3)
+    self.assertEqual("""\
+<table cellspacing="0" cellpadding="0" border="0" style="width:50ex">
+<tr>
+  <th style="margin-top:.2ex;padding-right:1ex;text-align:right;">my_label</th>
+  <td style="margin-top:.2ex;">
+    <table cellspacing="0" cellpadding="0" border="0" style="width:100%">
+    <tr>
+      <td style="width:33%;height:1ex;background-color:blue;"></td>
+      <td style="width:33%;height:1ex;background-color:blue;text-align:left">|</td>
+      <td style="width:33%;height:1ex;text-align:right">|</td>
+      <td></td>
+    </tr>
+    </table>
+  </td>
+</tr>
 </table>""",htmlSnippet)
 
+
+    
 if __name__ == '__main__':
   unittest.main()
